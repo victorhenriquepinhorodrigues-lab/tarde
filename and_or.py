dc02c43c0d84304d8802622813e6f624f8d8ab16
Victor <victorhenriquepinhorodrigues@gmail.com>
2026-03-24 15:34:31 -0300

    atualizando repositorio

--- and_or.py
+++ and_or.py
@@ -1,11 +1,23 @@
 """
-
-
-
-
-
-
+> MAIOR
+< MENOR
+>= MAIOR OU IGUAL
+<= MENOR OU IGUAL
+== IGUAL
+!= DIFERENTE
 """
+# or
+idade = 17
+autorizacao = "sim"
 
-print(10>5 or 10<5)
+if (idade >= 18 or autorizacao == "sim"):
+    print("Bem Vindo")
 
+# and
+login = "admin"
+senha = "123456"
+
+if (login == "admin" and senha == "165456"):
+    print("Acesso permitido")
+else:
+    print("Você não tem acesso permitido")
